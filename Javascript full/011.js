0c024c28a0705909f0979a1214bc405d6bcc8f9e
Immortal <ak2061435@gmail.com>
2022-11-17 18:40:57 +0530

    deleted directory

--- Javascript full/011.js
+++
@@ -1,9 +0,0 @@
-console.log('yes this is 11 you can start coding');
-setTimeout(() => {
-    
-    for (let index = 0; index < 4000; index++) {
-        console.log('printinng ',index);
-         
-        }
-}, 2000);
-console.log(' printin completed ')
